fb68bd171299862b7d8ab33612cf9b2cc6b86cd4
Tim Chambers <tim@timchambers.com>
2019-01-23 17:08:01 -0500

    Remove uneeded prompt

--- prompts.js
+++ prompts.js
@@ -5,12 +5,6 @@ module.exports = [
     type: 'confirm'
   },
   {
-    name: 'uikitRemove',
-    when: answers => !answers.uikit,
-    message: `🚨 Since this plugin is unused, you should remove it with the command which will be printed below.`,
-    type: 'confirm'
-  },
-  {
     name: 'uikitIcons',
     when: answers => !!answers.uikit,
     message: 'Do you want to include the UIKit icons in your project?',
